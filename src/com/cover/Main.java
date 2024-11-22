6729898d5f1df28490c7b656da554e96d9e93bda
fraigneau <fraigneau86@yahoo.fr>
2024-11-22 14:43:04 +0100

    Refactor Main and SendImage classes to enhance image processing flow; introduce dynamic file path handling and clean up commented code

--- src/com/cover/Main.java
+++ src/com/cover/Main.java
@@ -2,12 +2,31 @@ package com.cover;
 
 public class Main {
 
+    public static String Photo = "photo/";
+    public static String Resize = "Resize/";
+    public static String Bin = "Bin/";
+
+    public static String jpeg = ".jpg";
+    public static String bmp = ".bmp";
+    public static String bin = ".bin";
+
+    public static String in = "1";
+    public static String mid = "resize";
+
     public static void main(String[] args) {
 
-        Resize resize = new Resize();
-        resize.Image("test.jpeg", "eightbit.bmp");
-        SendImage sendImage = new SendImage();
-        sendImage.sendImage("eightbit.bmp", "pixels.bin");
+        for (int i = 1; i < 2; i++) {
+            in = Integer.toString(i);
+            resizeImage();
+        }
+    }
+
+    public static void resizeImage() {
+
+            Resize resize = new Resize();
+            resize.Image((Photo + in + jpeg), (Resize + mid + bmp));
+            SendImage sendImage = new SendImage();
+            sendImage.sendImage((Resize + mid + bmp), (Bin + in + bin));
     }
 
 }
--- src/com/cover/SendImage.java
+++ src/com/cover/SendImage.java
@@ -6,32 +6,27 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import java.util.zip.CRC32;
+//import java.util.zip.CRC32;
 
 public class SendImage {
     public void sendImage(String in, String out) {
         try {
             BufferedImage image = ImageIO.read(new File(in));
-            int[][] hexArray = new int[240][240];
+            //int[][] hexArray = new int[240][240];
             FileOutputStream fos = new FileOutputStream(out, false);
-            CRC32 crc = new CRC32();
-            
+            //CRC32 crc = new CRC32();
+
             fos.write("PetitHeaderLoveU".getBytes());
             for (int y = 0; y < 240; y++) {
                 for (int x = 0; x < 240; x++) {
                     int rgb = image.getRGB(x, y);
-                    //System.err.println(rgb);
                     Color color = new Color(rgb);
 
-                    //rgb = (color.getRed() << 16) | (color.getGreen() << 8) | color.getBlue();
-                    int red = (color.getRed() >> 16) ;
-                    int green = (color.getGreen() >> 8);
+                    int red = (color.getRed());
+                    int green = (color.getGreen());
                     int blue = color.getBlue();
                     if (x == 1 && y == 1) {
-                        System.err.println("red: " + red);
-                        System.err.println("green: " + green);
-                        System.err.println("blue: " + blue);
-                        
+                        System.err.println("red: " + red + " green: " + green + " blue: " + blue);
                     }
                     fos.write(red);
                     fos.write(green);
