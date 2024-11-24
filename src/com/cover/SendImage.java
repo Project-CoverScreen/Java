d5564001144ac93334a294dbcd33f52db39d33ba
fraigneau <fraigneau86@yahoo.fr>
2024-11-24 14:43:45 +0100

    Refactor Main and SendImage classes; limit image processing loop, remove CRC class, and enhance CRC calculation in SendImage

--- src/com/cover/SendImage.java
+++ src/com/cover/SendImage.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-//import java.util.zip.CRC32;
+import java.util.zip.CRC32;
 
 public class SendImage {
     public void sendImage(String in, String out) {
@@ -14,25 +14,30 @@ public class SendImage {
             BufferedImage image = ImageIO.read(new File(in));
             //int[][] hexArray = new int[240][240];
             FileOutputStream fos = new FileOutputStream(out, false);
-            //CRC32 crc = new CRC32();
+            CRC32 crc = new CRC32();
 
             fos.write("PetitHeaderLoveU".getBytes());
             for (int y = 0; y < 240; y++) {
                 for (int x = 0; x < 240; x++) {
                     int rgb = image.getRGB(x, y);
                     Color color = new Color(rgb);
-
-                    int red = (color.getRed());
-                    int green = (color.getGreen());
-                    int blue = color.getBlue();
-                    if (x == 1 && y == 1) {
-                        System.err.println("red: " + red + " green: " + green + " blue: " + blue);
-                    }
-                    fos.write(red);
-                    fos.write(green);
-                    fos.write(blue);
-                }
+                    
+                    fos.write(color.getRed());
+                    fos.write(color.getGreen());
+                    fos.write(color.getBlue());
+                    crc.update(color.getRed() + color.getGreen() + color.getBlue());
+             }
             }
+            //  write crc to file as int in 4 bytes please
+            int crcValue = (int) crc.getValue();
+            fos.write((crcValue >> 24) & 0xFF);
+            fos.write((crcValue >> 16) & 0xFF);
+            fos.write((crcValue >> 8) & 0xFF);
+            fos.write(crcValue & 0xFF);
+
+            // print crc
+            System.out.println("CRC: " + crcValue);
+
             fos.write("PetitFootercFini".getBytes());
             fos.close();
         } catch (IOException e) {
